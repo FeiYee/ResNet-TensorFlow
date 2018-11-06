4a85281bed5e3bbae3b86f2a0ce4db84f5697659
FeiYee <36070759+FeiYee@users.noreply.github.com>
2018-11-06 21:31:07 +0800

    Update ResNet_lib.py

--- ResNet_lib.py
+++ ResNet_lib.py
@@ -64,10 +64,11 @@ def sampling(input_tensor,      #Tensor入口
 
 
 
-def conv2d_same(input_tensor,num_outputs,kernel_size,stride,is_train = True,activation_fn=tf.nn.relu,normalizer_fn = True):
+def conv2d_same(input_tensor,num_outputs,kernel_size,stride,is_train = True,activation_fn=tf.nn.relu,normalizer_fn = True,scope = None):
     data = input_tensor
     if stride is 1:
-        data = slim.conv2d(inputs = data,num_outputs = num_outputs,kernel_size = kernel_size,stride = stride,weights_regularizer=slim.l2_regularizer(WEIGHT_DECAY),activation_fn=None,padding='SAME')
+        data = slim.conv2d(inputs = data,num_outputs = num_outputs,kernel_size = kernel_size,stride = stride,
+                           weights_regularizer=slim.l2_regularizer(WEIGHT_DECAY),activation_fn=None,padding='SAME',scope=scope)
     else:
 
         pad_total = kernel_size - 1
@@ -75,7 +76,8 @@ def conv2d_same(input_tensor,num_outputs,kernel_size,stride,is_train = True,acti
         pad_end = pad_total - pad_beg
         data = tf.pad(data,
                         [[0, 0], [pad_beg, pad_end], [pad_beg, pad_end], [0, 0]])
-        data = slim.conv2d(inputs = data,num_outputs = num_outputs,kernel_size = kernel_size,stride = stride,weights_regularizer=slim.l2_regularizer(WEIGHT_DECAY),activation_fn=None,padding='VALID')
+        data = slim.conv2d(inputs = data,num_outputs = num_outputs,kernel_size = kernel_size,stride = stride,
+                           weights_regularizer=slim.l2_regularizer(WEIGHT_DECAY),activation_fn=None,padding='VALID',scope=scope)
 
     print("Conv ",kernel_size, "depth = ", num_outputs, "  stride = ", stride)
     if normalizer_fn:
@@ -103,11 +105,11 @@ def bottleneck(input_tensor,output_depth,stride,is_train):
         #通道改变
         shortcut_tensor = highway(input_tensor,output_depth,is_train)
     #降通道处理
-    data = conv2d_same(data,output_depth//4,1,1,is_train)
+    data = conv2d_same(data,output_depth//4,1,1,is_train,scope='conv1_1x1')
     #提取特征
-    data = conv2d_same(data,output_depth//4,3,stride,is_train)
+    data = conv2d_same(data,output_depth//4,3,stride,is_train,scope='conv2_3x3')
     #通道还原
-    data = conv2d_same(data,output_depth,1,1,is_train,None,normalizer_fn = False)
+    data = conv2d_same(data,output_depth,1,1,is_train,activation_fn = None,normalizer_fn = False,scope='conv3_1x1')
     #生成残差
     data = data + shortcut_tensor
     data = tf.nn.relu(data)
@@ -125,33 +127,36 @@ def inference(input_tensor,  #数据入口
     data = slim.max_pool2d(data,3,2,scope="pool_1")
 
     with tf.variable_scope("resnet"):
+    # with tf.variable_scope("resnet"):
         #堆叠总类瓶颈模块
         demo_num = 0
         for demo in demos:
             demo_num += 1
             print("--------------------------------------------")
             #堆叠子类瓶颈模块
-            for i in range(demo["num_class"]):
-                print(demo_num)
-                if demo_num is not 4:
-                    if i == demo["num_class"] - 1:
-                        stride = 2
+            with tf.variable_scope("num_" + str(demo_num)):
+                for i in range(demo["num_class"]):
+                    print(demo_num)
+                    if demo_num is not 4:
+                        if i == demo["num_class"] - 1:
+                            stride = 2
+                        else:
+                            stride = 1
                     else:
                         stride = 1
-                else:
-                    stride = 1
-                data = bottleneck(data,demo["depth"],stride,is_train)
+                    with tf.variable_scope("bottleneck_" + str(i + 1)):
+                        data = bottleneck(data,demo["depth"],stride,is_train)
 
             print("--------------------------------------------")
     data = tf.layers.batch_normalization(data,training=is_train)
     data = tf.nn.relu(data)
     #平均池化，也可用Avg_pool函数
-    data = tf.reduce_mean(data, [1, 2], keep_dims=True)
+    data = tf.reduce_mean(data, [1, 2], keep_dims=True,name='nal_pool')
     #data = slim.avg_pool2d(data,2)
     print("output : ", data)
 
     #最后全连接层
-    data = slim.conv2d(data,num_output,1,activation_fn=None)
+    data = slim.conv2d(data,num_output,1,activation_fn=None,scope='final_conv')
 
     data_shape = data.get_shape().as_list()
     nodes = data_shape[1] * data_shape[2] * data_shape[3]
